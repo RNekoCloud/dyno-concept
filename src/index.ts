cdd78c04dd57e9cd83ceca3f0dfb2e7b80a36adf
RageNeko26 <rneko2006@gmail.com>
2024-01-19 12:53:19 +0700

    feat: Initialize dyno params for getting data

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import AWS from "aws-sdk";
+import DynoParams from "./types/dyno_param";
 
 // Load up shared configuration AWS in local machine
-const credentials = new AWS.SharedIniFileCredentials()
-AWS.config.credentials = credentials
+const credentials = new AWS.SharedIniFileCredentials();
+AWS.config.credentials = credentials;
 
 // Set region into config
-AWS.config.update({region: "us-east-1"})
+AWS.config.update({region: "us-east-1"});
+console.log("Hello from AWS SDK...");
 
-console.log("Hello from AWS SDK...")
+// Setup DynamoDB
+const dyno = new AWS.DynamoDB({ apiVersion: "2012-08-10"})
+
+// Getting data by params
+const params: DynoParams = {
+    TableName: "todo_app",
+    Key: {
+        ID: {
+            N: "1"
+        },
+    },
+    ProjectionExpression: "",
+}
