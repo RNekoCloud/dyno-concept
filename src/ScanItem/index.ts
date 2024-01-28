a715927b9d6867f17d71ba0d2cecce8ec831ad3d
RageNeko26 <rneko2006@gmail.com>
2024-01-28 20:19:11 +0700

    fix: Add last evaluated for pagination

--- src/ScanItem/index.ts
+++ src/ScanItem/index.ts
@@ -3,12 +3,14 @@ import { DynoClient } from "../../constant/config";
 
 const init = async() => {
     const input: ScanCommandInput = {
-        TableName: "notes"
+        TableName: "notes",
+        Limit: 3
     }
 
     const cmd = new ScanCommand(input);
     const response = await DynoClient.send(cmd)
     console.log(response.Items)
+    console.log(`Last Evaluated Key: ${response.LastEvaluatedKey?.noteid.S}`)
 
 };
 
